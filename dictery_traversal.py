fe438f5cb13206f1f4faac5a42626b5b848ea3de
Miguel Ángel <44236873+miguelpantoja89@users.noreply.github.com>
2022-11-02 18:43:53 +0100

    Create dictery_traversal.py

---
+++ dictery_traversal.py
@@ -0,0 +1,7 @@
+# directory_traversal.py
+import os
+from os import system
+BASE_PATH = './documents/'
+filename = input('\nEnter the file name: ')
+filename = os.path.join(BASE_PATH, filename)
+system('cat ' + filename)
